5f9bac99ba5548449d18fc6ec67d25f299b366d9
Marcelo de Moraes Andrade <47828310+MM-Andrade@users.noreply.github.com>
2023-10-16 19:29:39 -0300

    :test_tube: tests: Unit Test

---
+++ RestApi.tests/TestHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace RestApi.tests
+{
+    public class TestHelpers
+    {
+        private const int _expectedMaxElapsedMilliseconds = 1000;
+        public const string _jsonMediaType = "application/json";
+        public static readonly JsonSerializerOptions _jsonSerializerOptions = new() { PropertyNameCaseInsensitive = true };
+
+        public static async Task AssertResponseWithContentAsync<T>(Stopwatch stopwatch, HttpResponseMessage response, HttpStatusCode expectedStatusCode, T expectedContent)
+        {
+            AssertCommonResponseParts(stopwatch, response, expectedStatusCode);
+            Assert.Equal(_jsonMediaType, response.Content.Headers.ContentType?.MediaType);
+            Assert.Equal(expectedContent, await JsonSerializer.DeserializeAsync<T?>(await response.Content.ReadAsStreamAsync(), _jsonSerializerOptions));
+
+        }
+
+        public static void AssertCommonResponseParts(Stopwatch stopwatch, HttpResponseMessage response, HttpStatusCode expectedStatusCode)
+        {
+            Assert.Equal(expectedStatusCode, response.StatusCode);
+            Assert.True(stopwatch.ElapsedMilliseconds < _expectedMaxElapsedMilliseconds);
+        }
+
+        public static StringContent GetJsonStringContent<T>(T content) => new StringContent(JsonSerializer.Serialize(content), Encoding.UTF8, _jsonMediaType);
+        
+    }
+}
--- RestApi/Program.cs
+++ RestApi/Program.cs
@@ -1,44 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var books = new List<Book>();
 
-app.MapGet("/weatherforecast", () =>
+void InitializeBooks() => books = Enumerable.Range(1, 5)
+    .Select(index => new Book(index, $"Awesome book #{index}"))
+    .ToList();
+
+InitializeBooks();
+
+app.MapGet("/books", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    return Results.Ok(books);
+});
+app.MapPost("/books", (Book book) =>
+{
+    books.Add(book);
+    return Results.Created($"/books/{book.BookId}", book);
+});
+app.MapPut("/books", (Book book) =>
+{
+    books.RemoveAll(book => book.BookId == book.BookId);
+    books.Add(book);
+    return Results.Ok(book);
+});
+app.MapDelete("/books/{bookId}", (int bookId) =>
+{
+    books.RemoveAll(book => book.BookId == bookId);
+    return Results.NoContent();
+});
+
+app.MapGet("/admin", ([FromHeader(Name = "X-Api-Key")] string apiKey) =>
+{
+    if (apiKey == "SuperSecretApiKey")
+    {
+        return Results.Ok("Hi admin!");
+    }
+    return Results.Unauthorized();
+});
+
+app.MapDelete("/state", () =>
+{
+    InitializeBooks();
+    return Results.NoContent();
+});
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+internal record Book(int BookId, string Title);
